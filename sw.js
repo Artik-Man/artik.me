9198fb41028e72c04b7fbdbddd482eb8d18ef6f0
Artik <artik.alpha@gmail.com>
2019-01-08 13:29:05 +0300

    FIX: service workers and app name

--- sw.js
+++ sw.js
@@ -1,20 +1,33 @@
 const cacheName = 'cache-v1';
 const precacheResources = [
-  'index.html',
-  'styles/styles.css',
-  'styles/images/me.webp',
-  'styles/images/projects/kaspersky.webp',
-  'styles/images/projects/kmp.webp',
-  'styles/images/projects/hashboard.webp',
-  'styles/images/projects/hashboard-cabinet.webp',
-  'styles/images/projects/hashboard-player.webp',
-  'styles/images/projects/m315.webp',
-  'styles/images/projects/pr.webp',
-  'styles/fonts/bebas/font/bebasneuethin.woff2',
-  'styles/fonts/bebas/font/bebasneuelight.woff2',
-  'styles/fonts/lato/fonts/Lato-Light.woff2',
-  'styles/fonts/lato/fonts/Lato-Regular.woff2',
-  'styles/fonts/lato/fonts/Lato-Thin.woff2',
+  '/index.html',
+  '/styles/styles.css',
+  '/app.js',
+  '/sw.js',
+  // images webp
+  '/styles/images/me.webp',
+  '/styles/images/projects/kaspersky.webp',
+  '/styles/images/projects/kmp.webp',
+  '/styles/images/projects/hashboard.webp',
+  '/styles/images/projects/hashboard-cabinet.webp',
+  '/styles/images/projects/hashboard-player.webp',
+  '/styles/images/projects/m315.webp',
+  '/styles/images/projects/pr.webp',
+  // images jpg
+  '/styles/images/me.jpg',
+  '/styles/images/projects/kaspersky.jpg',
+  '/styles/images/projects/kmp.jpg',
+  '/styles/images/projects/hashboard.jpg',
+  '/styles/images/projects/hashboard-cabinet.jpg',
+  '/styles/images/projects/hashboard-player.jpg',
+  '/styles/images/projects/m315.jpg',
+  '/styles/images/projects/pr.jpg',
+  // fonts
+  '/styles/fonts/bebas/font/bebasneuethin.woff2',
+  '/styles/fonts/bebas/font/bebasneuelight.woff2',
+  '/styles/fonts/lato/fonts/Lato-Light.woff2',
+  '/styles/fonts/lato/fonts/Lato-Regular.woff2',
+  '/styles/fonts/lato/fonts/Lato-Thin.woff2',
 ];
 
 self.addEventListener('install', event => {
