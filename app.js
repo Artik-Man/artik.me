8e9ae9e8b4ac153730421ff746f093d59d385f92
Artik <artik.alpha@gmail.com>
2019-01-08 20:11:36 +0300

    UPD: code style

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ if ('serviceWorker' in navigator) {
             // Service worker has been registered
         });
     }
-};
+}
 
 // Yandex.Metrika counter
 (function (m, e, t, r, i, k, a) {
     m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
     m[i].l = 1 * new Date();
     k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
 ym(35320200, "init", {
     id: 35320200,
