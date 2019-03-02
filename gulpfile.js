e4b987bb5dc30e400a6e7f142e5ba02ef3a8f026
Artik <artik.alpha@gmail.com>
2019-03-02 20:14:15 +0300

    UPD: gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,57 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const less = require('gulp-less');
 const cssmin = require('gulp-cssmin');
+const connect = require('gulp-connect');
 const sourcemaps = require('gulp-sourcemaps');
 const LessAutoprefix = require('less-plugin-autoprefix');
 
 gulp.task('less', () => {
+  console.log('task: less');
   const autoprefix = new LessAutoprefix({
     browsers: [
       '> 0.5%',
       'last 5 versions',
       'Firefox ESR',
       'not dead',
-      'IE 9-11'
-    ]
+      'IE 9-11',
+    ],
   });
-
-  return gulp.src('./styles/styles.less')
+  return gulp
+    .src('./styles/styles.less')
     .pipe(sourcemaps.init())
-    .pipe(less({
-      plugins: [autoprefix]
-    }))
+    .pipe(
+      less({
+        plugins: [autoprefix],
+      })
+    )
     .pipe(cssmin())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./styles/'));
 });
 
 gulp.task('pug', () => {
-  return gulp.src('./*.pug')
+  console.log('task: pug');
+  return gulp
+    .src('./*.pug')
     .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest(file => './'))
+    .pipe(gulp.dest(file => './'));
 });
 
+gulp.task('webserver', () =>
+  connect.server({
+    root: './',
+    livereload: true,
+    port: parseInt(process.env.PORT) || 8080,
+    host: 'localhost',
+  })
+);
+
+gulp.task('watch', () => {
+  console.log('task: watch');
+  gulp.watch('./styles/*.less', gulp.series('less'));
+  gulp.watch('./*.pug', gulp.series('pug'));
+});
 
 gulp.task('default', gulp.parallel('less', 'pug'));
+
+gulp.task('serve', gulp.parallel('default', 'webserver', 'watch'));
