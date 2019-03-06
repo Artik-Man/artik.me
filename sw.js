358a81eb27a165b50b24feda48eb9c56ec6a75ca
Artik <artik.alpha@gmail.com>
2019-03-06 22:27:47 +0300

    UPD: ServiceWorker

--- sw.js
+++ sw.js
@@ -1,28 +1,26 @@
-var CACHE = 'precache-v2';
+var CACHE = 'precache-v3';
 
-const icons = [16, 48, 72, 76, 96, 120, 128, 144, 152, 180, 192, 256, 384, 512].map(item => 'styles/images/icons/icon-' + item + 'x' + item + '.png');
-const jpg = ['styles/images/me.jpg',
+const icons = [128, 192, 256, 384, 512].map(item => './styles/images/icons/icon-' + item + 'x' + item + '.png');
+const jpg = [
+  'styles/images/me.jpg',
   'styles/images/projects/kaspersky.jpg',
+  'styles/images/projects/artik.jpg',
   'styles/images/projects/kmp.jpg',
   'styles/images/projects/hashboard.jpg',
   'styles/images/projects/hashboard-cabinet.jpg',
   'styles/images/projects/hashboard-player.jpg',
   'styles/images/projects/m315.jpg',
-  'styles/images/projects/pr.jpg'];
+  'styles/images/projects/pr.jpg'
+];
 const webp = jpg.map(item => item.replace(/jpg$/, 'webp'));
 
 var precacheFiles = [
-  './',
-  './index.html',
-  './styles/fonts/bebas/font.css',
-  './styles/fonts/lato/font.css',
-  './styles/styles.css',
-  './app.js',
-  './styles/fonts/bebas/font/bebasneuethin.woff2',
-  './styles/fonts/bebas/font/bebasneuelight.woff2',
-  './styles/fonts/lato/font/Lato-Light.woff2',
-  './styles/fonts/lato/font/Lato-Regular.woff2',
-  './styles/fonts/lato/font/Lato-Thin.woff2',
+  'index.html',
+  'styles/fonts/bebas/font.css',
+  'styles/fonts/lato/font.css',
+  'styles/styles.css',
+  'app.js',
+  'styles/fonts/bebas/font/bebasneuelight.woff2'
 ]
   .concat(icons)
   .concat(jpg)
@@ -66,20 +64,57 @@ function fromServer(request) {
     .catch(e => console.warn(e));
 }
 
-self.addEventListener('install', e => {
-  e.waitUntil(precache()
-    .then(() => {
-      return self.skipWaiting();
-    })
-    .catch(e => console.warn(e))
-  );
-});
+// self.addEventListener('install', e => {
+//   e.waitUntil(precache()
+//     .then(() => {
+//       return self.skipWaiting();
+//     })
+//     .catch(e => console.warn(e))
+//   );
+// });
 
-self.addEventListener('activate', e => {
-  return self.clients.claim();
-});
+// self.addEventListener('activate', e => {
+//   return self.clients.claim();
+// });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fromCache(e.request).catch(fromServer(e.request)));
-  e.waitUntil(update(e.request));
+// self.addEventListener('fetch', event => {
+//   console.log(event);
+
+//   // e.respondWith(fromCache(e.request).catch(fromServer(e.request)));
+//   // e.waitUntil(update(e.request));
+
+//   event.respondWith(async () => {
+//     const request = event.request;
+//     const cachedResponse = await caches.open(CACHE).then(cache => cache.match(request));
+//     // console.log(cachedResponse);
+//     // if (cachedResponse) {
+//     //   return cachedResponse;
+//     // }
+
+//     // const response = await fetch(request);
+//     // caches.open(CACHE).then(cache => cache.put(request, response));
+//     // console.log(response);
+//     // return response;
+
+//   })
+// });
+
+self.addEventListener('fetch', function (event) {
+  const request = event.request;
+  // const canBePreCached = precacheFiles.find(file => {
+  //   return request.url.indexOf(file) !== -1;
+  // });
+  const canBePreCached = request.url.indexOf('https://artik.me') === 0;
+  if (canBePreCached) {
+    event.respondWith(
+      caches.match(request).then(function (resp) {
+        return resp || fetch(request).then(function (response) {
+          return caches.open(CACHE).then(function (cache) {
+            cache.put(request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  }
 });
